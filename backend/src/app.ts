19a6d055d69975c539081caa6ed4d291f1277135
Angie Samson <m.angelinesamson@gmail.com>
2026-05-11 21:32:36 +0800

    [Fix]: Add coookie parser in app.ts middleware.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { config } from "dotenv";
 import appRouter from "./routes/appRoutes";
+import cookieParser from "cookie-parser";
 
 config();
 const app = express();
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use("/api/v1", appRouter);
 
