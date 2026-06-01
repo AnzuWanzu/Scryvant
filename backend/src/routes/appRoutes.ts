9308e1a696fe735416e13e1cdbbd004552efc766
Angie Samson <m.angelinesamson@gmail.com>
2026-06-01 21:41:15 +0800

    [Feature]: Create functions for getting and creating user character sheets.

--- backend/src/routes/appRoutes.ts
+++ backend/src/routes/appRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes";
+import characterRoutes from "./characterRoutes";
 
 const appRouter = Router();
 
 appRouter.use("/user", userRoutes);
+appRouter.use("/character", characterRoutes);
 
 export default appRouter;
